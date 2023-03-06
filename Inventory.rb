e167db9ca531b8f012c6a8a7c4fcd2f07c39e759
Karina Sasaki <karinasasaki99@gmail.com>
2023-03-06 16:10:00 -0400

    Ajustes

--- Inventory.rb
+++ Inventory.rb
@@ -5,25 +5,51 @@ class Inventory
     end
     
     def add_item
-        puts "\n"
-        print "Nome: "
-        name = gets.chomp
-        print "Quantidade: "
-        amount = gets.chomp.to_i
-        print "Preço unitário: " 
-        price = gets.chomp.to_f
+        name = ""
+        amount = 0
+        price = 0
 
+        until name.empty? == false do
+            puts "\n"
+            print "Nome: "
+            name = gets.chomp
+            if name.empty? == true
+                puts "\n"
+                puts "ERRO! O nome não deve ser vazio, digite novamente."
+            end
+        end
+
+        until amount > 0 do
+            puts "\n"
+            print "Quantidade: "
+            amount = gets.chomp.to_i
+            if amount <= 0
+                puts "\n"
+                puts "ERRO! A quantidade deve ser maior que 0, digite novamente."
+            end
+        end
+
+        until price > 0 do
+            puts "\n"
+            print "Preço unitário: " 
+            price = gets.chomp.to_f
+            if price <= 0
+                puts "\n"
+                puts "ERRO! O preço deve ser maior que 0, digite novamente."
+            end
+        end
+        
         @products << Product.new(name, amount, price)
         
         puts "\n"
-        puts "Item adicionado!"
+        puts "Item adicionado com sucesso!"
     end
     
     def remove_item
         puts "\n"
         print "Qual produto deseja excluir? "
         product = gets.chomp
-        status = false
+        productWasDeleted = false
 
         @products.each do |item|
             if item.name.downcase.include?(product.downcase)
@@ -31,11 +57,11 @@ class Inventory
                 @products.delete_at(id)
                 puts "\n"
                 puts "#{item.name} excluído do estoque!"
-                status = true
+                productWasDeleted = true
             end
         end
     
-        if status == false
+        if productWasDeleted == false
             puts "\n"
             puts "Produto não encontrado."
         end
@@ -60,7 +86,7 @@ class Inventory
         puts "\n"
         print "Qual produto deseja visualizar? "
         product = gets.chomp
-        status = false
+        productNotFound = true
     
         @products.each do |item|
             if item.name.downcase.include?(product.downcase)
@@ -70,11 +96,11 @@ class Inventory
                 puts "Quantidade: #{item.amount}\n"
                 puts "Preço unitário: R$#{item.price}\n"
                 puts "Valor total do item no estoque: R$#{item.totalValue()}"
-                status = true
+                productNotFound = false
             end
         end
     
-        if status == false
+        if productNotFound == true
             puts "\n"
             puts "Produto não encontrado."
         end
